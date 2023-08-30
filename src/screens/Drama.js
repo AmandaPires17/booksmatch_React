982282141b4683f75c373d4e4112bcf1463b7723
AmandaPires17 <nanapires255@gmail.com>
2023-08-30 11:48:18 -0300

    drama

--- src/screens/Drama.js
+++ src/screens/Drama.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { StyleSheet, View, Text, Image, ScrollView } from 'react-native';
+import api from '../plugins/api';
 
 const Item = (props) => {
   return (
@@ -15,44 +16,58 @@ const Item = (props) => {
 }
 
 export default function Drama() {
-  const dramas = [
-    {
-      id: 1,
-      capa: 'https://m.media-amazon.com/images/I/51wdOrz6uNL._SX340_BO1,204,203,200_.jpg',
-      titulo: '"O sol é para todos"',
-      preco: 36.7,
-    },
-    {
-      id: 2,
-      capa: 'https://m.media-amazon.com/images/I/413th2NQc8L._SY344_BO1,204,203,200_QL70_ML2_.jpg',
-      titulo: '"A paciente silenciosa"',
-      preco: 33.9,
-    },
-    {
-      id: 3,
-      capa: 'https://m.media-amazon.com/images/I/41MRn6hy8-L._SY344_BO1,204,203,200_QL70_ML2_.jpg',
-      titulo: '"Duna: livro 1"',
-      preco: 47.99,
-    },
-    {
-      id: 4,
-      capa: 'https://m.media-amazon.com/images/I/51Ooch+98bL._SY344_BO1,204,203,200_.jpg',
-      titulo: '"O apanhador no campo"',
-      preco: 48.9,
-    },
-    {
-      id: 5,
-      capa: 'https://m.media-amazon.com/images/I/41MeSSE9I-L.jpg',
-      titulo: '"Um inimigo do povo"',
-      preco: 17.9,
-    },
-    {
-      id: 6,
-      capa: 'https://m.media-amazon.com/images/I/41oMkonRbTL._SX331_BO1,204,203,200_.jpg',
-      titulo: '"Hamlet"',
-      preco: 14.99,
-    },
-  ];
+
+  const [ dramas, setDramas ] = useState([])
+
+  async function getLivros() {
+    const { data } = await api.get('/livros/')
+    console.log(data)
+    setDramas(data)
+  }
+
+  useEffect(() => {
+    getLivros()
+  }, [])
+
+
+  //const dramas = [
+  //  {
+  //    id: 1,
+  //    capa: 'https://m.media-amazon.com/images/I/51wdOrz6uNL._SX340_BO1,204,203,200_.jpg',
+  //    titulo: '"O sol é para todos"',
+  //    preco: 36.7,
+  //  },
+  //  {
+  //    id: 2,
+  //    capa: 'https://m.media-amazon.com/images/I/413th2NQc8L._SY344_BO1,204,203,200_QL70_ML2_.jpg',
+  //    titulo: '"A paciente silenciosa"',
+  //    preco: 33.9,
+  //  },
+  //  {
+  //    id: 3,
+  //    capa: 'https://m.media-amazon.com/images/I/41MRn6hy8-L._SY344_BO1,204,203,200_QL70_ML2_.jpg',
+  //    titulo: '"Duna: livro 1"',
+  //    preco: 47.99,
+  //  },
+  //  {
+  //    id: 4,
+  //    capa: 'https://m.media-amazon.com/images/I/51Ooch+98bL._SY344_BO1,204,203,200_.jpg',
+  //    titulo: '"O apanhador no campo"',
+  //    preco: 48.9,
+  //  },
+  //  {
+  //    id: 5,
+  //    capa: 'https://m.media-amazon.com/images/I/41MeSSE9I-L.jpg',
+  //    titulo: '"Um inimigo do povo"',
+  //    preco: 17.9,
+  //  },
+  //  {
+  //    id: 6,
+  //    capa: 'https://m.media-amazon.com/images/I/41oMkonRbTL._SX331_BO1,204,203,200_.jpg',
+  //    titulo: '"Hamlet"',
+  //    preco: 14.99,
+  //  },
+  //];
 
   return (
     <View style={styles.container}>
