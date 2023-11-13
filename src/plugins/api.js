0702d2ca897515e873bcf5ca789b17c3696013dc
AmandaPires17 <nanapires255@gmail.com>
2023-11-13 11:12:18 -0300

    alterações 13/11

--- src/plugins/api.js
+++ src/plugins/api.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 const { MY_IP } = process.env;
 
 const api = axios.create({
-  baseURL: `http://191.52.55.53:19003/`,
+  // baseURL: `http://${MY_IP}:19003/`,
+  baseURL: `https://booksmatch.3.us-1.fl0.io/`,
 });
 
 export default api;
